bb29f4ee8a9626073517a10b8fb0d5543648b151
Orsova <127113841+Orsova@users.noreply.github.com>
2023-12-13 01:34:04 +1100

    Update screenlock.py

--- screenlock.py
+++ screenlock.py
@@ -5,8 +5,13 @@ import time
 def time_in_range(start, end, current):
     return start <= current <= end
 
-start = datetime.time(20, 0, 0)
-end = datetime.time(7, 0, 0)
+# set first times for before midnight, second times for after midnight.
+
+start = datetime.time(22, 30, 0)
+end = datetime.time(23, 59, 59)
+
+start2 = datetime.time(0, 0, 0)
+end2 = datetime.time(7, 0, 0)
 
 while True:
 
@@ -17,7 +22,13 @@ while True:
         # print(bool(time_in_range(start, end, current)))
         loginPF = CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login')
         result = loginPF.SACLockScreenImmediate()
-        time.sleep(5)
+        time.sleep(15)
+    elif time_in_range(start2, end2, current):
+        # print(current)
+        # print(bool(time_in_range(start2, end2, current)))
+        loginPF = CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login')
+        result = loginPF.SACLockScreenImmediate()
+        time.sleep(15)
     else:
         # print (current)
         # print(bool(time_in_range(start, end, current)))
